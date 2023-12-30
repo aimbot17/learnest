cbe225aa5dc028ec3d2064f5ffc8e2d29ce2ca81
Utsav Joshi <joshiutsav56@gmail.com>
2023-12-30 14:59:01 +0530

    Added the backend for forget password functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,21 @@
-console.log("Hello world!");
+import express from "express";
+import cors from "cors";
 
-export function sum(a:number, b:number):number {
-  return a + b;
-}
+export const app = express();
+const PORT = 8000;
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+  res.status(200).send({ name: "test" });
+});
+
+app.post("/auth/forgot-password", (req, res) => {
+  const { email } = req.body;
+  res.status(200).send({ message: "Password reset email sent successfully!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
