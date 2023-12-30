1a97d4c0f9290a259acc08321c5fa820db79ec2d
Utsav Joshi <joshiutsav56@gmail.com>
2023-12-30 15:09:07 +0530

    corrected the prodution error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 
 export const app = express();
 const PORT = 8000;
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send({ name: "test" });
 });
 
-app.post("/auth/forgot-password", (req, res) => {
+app.post("/auth/forgot-password", (req: Request, res: Response) => {
   const { email } = req.body;
   res.status(200).send({ message: "Password reset email sent successfully!" });
 });
