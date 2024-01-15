46bf122632144abaca9e000d8c29d3ee8a466e45
Utsav Joshi <joshiutsav56@gmail.com>
2024-01-15 20:18:58 +0530

    routes

--- client/src/services/routes/routes.tsx
+++ client/src/services/routes/routes.tsx
@@ -86,7 +86,7 @@ const routes = createBrowserRouter([
     element: (
       <Suspense fallback={<div>Loading Videoplayer...</div>}>
         <PrivateRoutes>
-          <Videoplayer />
+          <VideoPlayer />
         </PrivateRoutes>
       </Suspense>
     ),
