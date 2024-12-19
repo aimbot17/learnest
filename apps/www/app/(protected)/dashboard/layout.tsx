c7c66774ed020b41275444b3f5b656b6b32cfda7
utsavjoshiNB <utsav.joshi@nextbill.io>
2024-12-19 22:18:37 +0530

    fix: added layout in dashboard

---
+++ apps/www/app/(protected)/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Dashboard",
+};
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return { children };
+}
